44f99fff0e8b817c18ddc5006696ffe3d9df8516
GOUNAICH <abdeslamgounaich61@gmail.com>
2024-04-08 20:49:02 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,297 @@
+const myslide = document.querySelectorAll('.myslide'),
+    dot = document.querySelectorAll('.dot');
+let counter = 1;
+slidefun(counter);
+
+let timer = setInterval(autoSlide, 8000);
+function autoSlide() {
+    counter += 1;
+    slidefun(counter);
+}
+function plusSlides(n) {
+    counter += n;
+    slidefun(counter);
+    resetTimer();
+}
+function currentSlide(n) {
+    counter = n;
+    slidefun(counter);
+    resetTimer();
+}
+function resetTimer() {
+    clearInterval(timer);
+    timer = setInterval(autoSlide, 8000);
+}
+
+function slidefun(n) {
+
+    let i;
+    for (i = 0; i < myslide.length; i++) {
+        myslide[i].style.display = "none";
+    }
+    for (i = 0; i < dot.length; i++) {
+        dot[i].className = dot[i].className.replace(' active', '');
+    }
+    if (n > myslide.length) {
+        counter = 1;
+    }
+    if (n < 1) {
+        counter = myslide.length;
+    }
+    myslide[counter - 1].style.display = "block";
+    dot[counter - 1].className += " active";
+}
+
+
+
+
+
+
+
+
+
+
+let dayBox = document.getElementById("day-box");
+let hrBox = document.getElementById("hr-box");
+let minBox = document.getElementById("min-box");
+let secBox = document.getElementById("sec-box");
+
+//Format: Date(year, month, day, hour, minute)
+//Year is counter from 0 to 11
+let endDate = new Date(2050, 8, 5, 16, 30);
+//Output value in milliseconds
+let endTime = endDate.getTime();
+
+function countdown() {
+    let todayDate = new Date();
+    //Output value in milliseconds
+    let todayTime = todayDate.getTime();
+
+    let remainingTime = endTime - todayTime;
+
+    //60sec => 1000 milliseconds
+    let oneMin = 60 * 1000;
+    //1hr => 60 minutes
+    let oneHr = 60 * oneMin;
+    //1 day => 24 hours
+    let oneDay = 24 * oneHr;
+
+    //Function to format number if it is single digit
+    let addZeroes = (num) => (num < 10 ? `0${num}` : num);
+
+    //If end dat is before today date
+    if (endTime < todayTime) {
+        clearInterval(i);
+        document.querySelector(
+            ".countdown"
+        ).innerHTML = `<h1>Countdown had expired!</h1>`;
+    }
+    //If end date is not before today date
+    else {
+        //Calculating remaining days, hrs,mins ,secs
+        let daysLeft = Math.floor(remainingTime / oneDay);
+        let hrsLeft = Math.floor((remainingTime % oneDay) / oneHr);
+        let minsLeft = Math.floor((remainingTime % oneHr) / oneMin);
+        let secsLeft = Math.floor((remainingTime % oneMin) / 1000);
+
+        //Displaying Valurs
+        dayBox.textContent = addZeroes(daysLeft);
+        hrBox.textContent = addZeroes(hrsLeft);
+        minBox.textContent = addZeroes(minsLeft);
+        secBox.textContent = addZeroes(secsLeft);
+    }
+}
+let i = setInterval(countdown, 1000);
+countdown();
+
+
+
+
+
+
+
+
+
+
+// Partie 2 : Fonctionnement du slider (carrousel d'images)
+(function () {
+    const wrapper = document.querySelector(".wrapper");
+    const carousel = document.querySelector(".carousel");
+    const firstCardWidth = carousel.querySelector(".card").offsetWidth;
+    const arrowBtns = document.querySelectorAll(".wrapper i");
+    const carouselChildrens = [...carousel.children];
+
+    let isDragging = false, isAutoPlay = true, startX, startScrollLeft, timeoutId;
+
+    // Get the number of cards that can fit in the carousel at once
+    let cardPerView = Math.round(carousel.offsetWidth / firstCardWidth);
+
+    // Insert copies of the last few cards to beginning of carousel for infinite scrolling
+    carouselChildrens.slice(-cardPerView).reverse().forEach(card => {
+        carousel.insertAdjacentHTML("afterbegin", card.outerHTML);
+    });
+
+    // Insert copies of the first few cards to end of carousel for infinite scrolling
+    carouselChildrens.slice(0, cardPerView).forEach(card => {
+        carousel.insertAdjacentHTML("beforeend", card.outerHTML);
+    });
+
+    // Scroll the carousel at appropriate postition to hide first few duplicate cards on Firefox
+    carousel.classList.add("no-transition");
+    carousel.scrollLeft = carousel.offsetWidth;
+    carousel.classList.remove("no-transition");
+
+    // Add event listeners for the arrow buttons to scroll the carousel left and right
+    arrowBtns.forEach(btn => {
+        btn.addEventListener("click", () => {
+            carousel.scrollLeft += btn.id == "left" ? -firstCardWidth : firstCardWidth;
+        });
+    });
+
+    const dragStart = (e) => {
+        isDragging = true;
+        carousel.classList.add("dragging");
+        // Records the initial cursor and scroll position of the carousel
+        startX = e.pageX;
+        startScrollLeft = carousel.scrollLeft;
+    }
+
+    const dragging = (e) => {
+        if (!isDragging) return; // if isDragging is false return from here
+        // Updates the scroll position of the carousel based on the cursor movement
+        carousel.scrollLeft = startScrollLeft - (e.pageX - startX);
+    }
+
+    const dragStop = () => {
+        isDragging = false;
+        carousel.classList.remove("dragging");
+    }
+
+    const infiniteScroll = () => {
+        // If the carousel is at the beginning, scroll to the end
+        if (carousel.scrollLeft === 0) {
+            carousel.classList.add("no-transition");
+            carousel.scrollLeft = carousel.scrollWidth - (2 * carousel.offsetWidth);
+            carousel.classList.remove("no-transition");
+        }
+        // If the carousel is at the end, scroll to the beginning
+        else if (Math.ceil(carousel.scrollLeft) === carousel.scrollWidth - carousel.offsetWidth) {
+            carousel.classList.add("no-transition");
+            carousel.scrollLeft = carousel.offsetWidth;
+            carousel.classList.remove("no-transition");
+        }
+
+        // Clear existing timeout & start autoplay if mouse is not hovering over carousel
+        clearTimeout(timeoutId);
+        if (!wrapper.matches(":hover")) autoPlay();
+    }
+
+    const autoPlay = () => {
+        if (window.innerWidth < 800 || !isAutoPlay) return; // Return if window is smaller than 800 or isAutoPlay is false
+        // Autoplay the carousel after every 2500 ms
+        timeoutId = setTimeout(() => carousel.scrollLeft += firstCardWidth, 2500);
+    }
+    autoPlay();
+
+    carousel.addEventListener("mousedown", dragStart);
+    carousel.addEventListener("mousemove", dragging);
+    document.addEventListener("mouseup", dragStop);
+    carousel.addEventListener("scroll", infiniteScroll);
+    wrapper.addEventListener("mouseenter", () => clearTimeout(timeoutId));
+    wrapper.addEventListener("mouseleave", autoPlay);
+})();
+
+
+
+
+
+
+
+
+
+
+
+
+
+(function () {
+    let tabs = document.querySelectorAll(".tabs h3");
+    let tabContents = document.querySelectorAll(".tab-content div");
+
+    tabs.forEach((tab, index) => {
+        tab.addEventListener("click", () => {
+            tabContents.forEach((content) => {
+                content.classList.remove("active");
+            });
+            tabs.forEach((tab) => {
+                tab.classList.remove("active");
+            });
+            tabContents[index].classList.add("active");
+            tabs[index].classList.add("active");
+        });
+    });
+})();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const questions = document.querySelectorAll('.question');
+
+questions.forEach(question => {
+    question.addEventListener('click', () => {
+        const answer = question.nextElementSibling;
+
+        if (answer.style.display === 'block') {
+            answer.style.display = 'none';
+        } else {
+            answer.style.display = 'block';
+        }
+    });
+});
+
+
+
+
+
+
+
+const accordionContent = document.querySelectorAll(".accordion-content");
+
+accordionContent.forEach((item, index) => {
+    let header = item.querySelector("header");
+    header.addEventListener("click", () => {
+        item.classList.toggle("open");
+
+        let description = item.querySelector(".description");
+        if (item.classList.contains("open")) {
+            description.style.height = `${description.scrollHeight}px`; //scrollHeight property returns the height of an element including padding , but excluding borders, scrollbar or margin
+            item.querySelector("i").classList.replace("fa-plus", "fa-minus");
+        } else {
+            description.style.height = "0px";
+            item.querySelector("i").classList.replace("fa-minus", "fa-plus");
+        }
+        removeOpen(index); //calling the funtion and also passing the index number of the clicked header
+    })
+})
+
+function removeOpen(index1) {
+    accordionContent.forEach((item2, index2) => {
+        if (index1 != index2) {
+            item2.classList.remove("open");
+
+            let des = item2.querySelector(".description");
+            des.style.height = "0px";
+            item2.querySelector("i").classList.replace("fa-minus", "fa-plus");
+        }
+    })
+}
